82d43d0dd1b3962fd3fe281b89d096fa0f072b02
t1dmlgus <dmlgusgngl@gmail.com>
2021-12-23 11:35:14 +0900

    init

---
+++ src/main/java/com/t1dmlgus/securityEx/SecurityExApplication.java
@@ -0,0 +1,13 @@
+package com.t1dmlgus.securityEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/t1dmlgus/securityEx/SecurityExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t1dmlgus.securityEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
